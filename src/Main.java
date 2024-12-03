20f8639582c4fb4c9a5c060e73af361d4cd9a346
Maria Lourdusamy <maria.lourdusamy@gmail.com>
2024-12-03 10:58:43 -0600

    Commit Employee Commission Project.

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+
+public class Main {
+       public static void main(String[] args) {
+
+        Employee_Comm empObj = new Employee_Comm(); //Create Employee object
+        empObj.inputEmployee(); //  Call the input Employee method
+       }
+}
